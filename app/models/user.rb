01d7616b6ad47e48e56d6d189a9a8a4e5ee3e353
“keyanestgit config --global user.email “keyan.est@gmail.com <YOUR@EMAIL.com>
2016-10-17 20:57:16 -0400

    Added validations to models. Added belongs_to/has_many to models. Updated schema: changed first_name/last_name to null: false for users table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  has_many :tasks
+  has_many :contacts
+  has_many :logs
+
+  validates_uniqueness_of :email
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
